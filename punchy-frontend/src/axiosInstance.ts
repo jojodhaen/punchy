7369d7557e1bfe99992e9c704e469974ecc3b440
joren.dhaen <joren.dhaen@student.odisee.be>
2024-07-04 23:12:48 +0200

    add backend

--- punchy-frontend/src/axiosInstance.ts
+++ punchy-frontend/src/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: '/api/',
+  // baseURL: '/api/',
+  baseURL: 'https://backend.punchy.jorendhaen.be',
   withCredentials: true,
   withXSRFToken: true,
   headers: {

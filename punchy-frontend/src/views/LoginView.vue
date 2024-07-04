4696690be0f551ae0d4be889978019ee651906b7
joren.dhaen <joren.dhaen@student.odisee.be>
2024-07-04 20:53:59 +0200

    Add loginflow

--- punchy-frontend/src/views/LoginView.vue
+++ punchy-frontend/src/views/LoginView.vue
@@ -18,24 +18,26 @@ const handleSubmit = (event: Event) => {
 axiosInstance.get('/sanctum/csrf-cookie').then((response) => {
   console.log(response.data)
 })
+
+function onTouchmove(e: TouchEvent) {
+  e.preventDefault()
+}
 </script>
 
 <template>
-  <h1>Login</h1>
-  <form v-on:submit="handleSubmit">
-    <label for="email">Email</label>
-    <input id="email" v-model="email" name="email" required type="email" />
-    <label for="password">Password</label>
-    <input id="password" v-model="password" name="password" required type="password" />
-    <button type="submit">Login</button>
-  </form>
+  <div @touchmove="onTouchmove">
+    <h1>Login</h1>
+    <form v-on:submit="handleSubmit">
+      <label for="email">Email</label>
+      <input id="email" v-model="email" name="email" required type="email" />
+      <label for="password">Password</label>
+      <input id="password" v-model="password" name="password" required type="password" />
+      <button type="submit">Login</button>
+    </form>
+  </div>
 </template>
 
 <style scoped>
-body {
-  touch-action: none;
-}
-
 form {
   display: flex;
   flex-direction: column;

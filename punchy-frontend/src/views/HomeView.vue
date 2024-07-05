e0896c13d0536899f5cad59aa46cd2669ef52437
joren.dhaen <joren.dhaen@student.odisee.be>
2024-07-05 23:02:57 +0200

    edit logos

--- punchy-frontend/src/views/HomeView.vue
+++ punchy-frontend/src/views/HomeView.vue
@@ -3,23 +3,61 @@ import { useAuthStore } from '@/stores/auth'
 
 const auth = useAuthStore()
 const user: User = auth.user ?? ({} as User)
+const week = ['MA', 'DI', 'WO', 'DO', 'VR', 'ZA', 'ZO']
 </script>
 
 <template>
   <main>
-    <p>Welkom {{ user.first_name }}</p>
+    <h1>Welkom {{ user.first_name }}</h1>
+    <div class="divider"></div>
+    <h2>Jouw werkweek</h2>
+    <div class="calendar-container">
+      <div v-for="day in week" class="day">
+        <p>{{ day }}</p>
+      </div>
+    </div>
+
     <button @click="auth.logout">Logout</button>
   </main>
 </template>
 
 <style scoped>
-p {
+h1 {
   font-family: 'Bodoni Moda SC', serif;
   font-size: 2rem;
   font-weight: 1000;
   line-height: 1;
   margin: 0;
-  padding: 1rem;
+  padding: 1rem 1rem 0 1rem;
   overflow-wrap: break-word;
+}
+
+h2 {
+  font-family: 'Bodoni Moda SC', serif;
+  font-size: 1.5rem;
+  font-weight: 1000;
+  margin: 1rem;
+}
+
+.divider {
+  border: 2px solid #a87676;
+  border-radius: 2rem;
+  margin: 1rem;
+}
+
+.calendar-container {
+  display: flex;
+  justify-content: space-between;
+  gap: 5px;
+  margin: 1rem;
+}
+
+.day {
+  background-color: #a87676;
+  height: 100px;
+  flex: 1;
+  border-radius: 0.5rem;
+  font-family: 'Inconsolata', 'Fira Mono', 'Source Code Pro', Monaco, Consolas, 'Lucida Console',
+    monospace;
 }
 </style>
